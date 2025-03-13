f0412a71319887ea4b761491d44a17cd65be71b5
Youssef Mikhail <youssef.mikhail@outlook.com>
2025-03-12 21:58:29 -0400

    First commit (rough outline)

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+# TODO: Load YAML file with resume content
+
+# TODO: Load the LaTeX template
+
+# TODO: Load/take as input job description
+
+# TODO: Establish connections to ollama model
+
+# TODO: Create queries and get responses
+
+# TODO: Substitute LLM responses in YAML data
+
+# TODO: Render LaTeX template
+
+# TODO: Generate PDF
